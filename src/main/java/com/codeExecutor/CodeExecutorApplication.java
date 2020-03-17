d2d7d57eb54825bff1fa579bdede30cca208b19a
Aditya <adityashekhar35@gmail.com>
2020-03-17 17:16:27 +0530

    inital commit

---
+++ src/main/java/com/codeExecutor/CodeExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.codeExecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeExecutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeExecutor/CodeExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeExecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
